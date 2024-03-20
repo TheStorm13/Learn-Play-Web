160c8e12cf681f123616fdd08da1c4bc27ced7c9
ilya_groza <ilya.groza13@gmail.com>
2024-03-20 19:10:58 +0400

    Initial commit

---
+++ src/main/java/ru/lp/learnandplay/LearnandplayApplication.java
@@ -0,0 +1,13 @@
+package ru.lp.learnandplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnandplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnandplayApplication.class, args);
+	}
+
+}
