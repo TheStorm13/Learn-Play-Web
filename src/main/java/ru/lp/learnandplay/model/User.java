6a0b9ed6b95a5fe79fe08cc0dea421d6985f4c79
ilya_groza <ilya.groza13@gmail.com>
2024-03-23 22:09:15 +0400

    add fields of User

--- src/main/java/ru/lp/learnandplay/model/User.java
+++ src/main/java/ru/lp/learnandplay/model/User.java
@@ -1,4 +1,15 @@
 package ru.lp.learnandplay.model;
 
+
+
 public class User {
+    private Long id;
+    private String name;
+    private String email;
+    //    private String password;
+    private int exp;
+    private boolean dailyQuest;
+    private float multiplier;
+    private int idFrame;
+
 }
