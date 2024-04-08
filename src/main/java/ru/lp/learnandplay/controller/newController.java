81d1e27ad47890d08af4ff23e7a232bc3e98bdbc
Ilya Groza <91952941+TheStorm13@users.noreply.github.com>
2024-04-08 19:51:38 +0400

    Update newController.java

--- src/main/java/ru/lp/learnandplay/controller/newController.java
+++ src/main/java/ru/lp/learnandplay/controller/newController.java
@@ -1,4 +1,18 @@
 package ru.lp.learnandplay.controller;
 
 public class newController {
+   @GetMapping("/")
+    public String getWelcomePage() {
+        return "home";
+    }
+
+    @GetMapping("/entry")
+    public String getEntryPage() {
+        return "entry";
+    }
+    @GetMapping("/g")
+    @ResponseBody
+    public test getG() {
+        return new test("1",1);
+    }
 }
