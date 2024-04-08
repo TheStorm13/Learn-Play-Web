4289fedc10e8b2d9fd9a7454989faaa3a9fcbac2
Ilya Groza <91952941+TheStorm13@users.noreply.github.com>
2024-04-08 21:32:55 +0400

    Update taskController.java

--- src/main/java/ru/lp/learnandplay/controller/taskController.java
+++ src/main/java/ru/lp/learnandplay/controller/taskController.java
@@ -1,16 +1,13 @@
 package ru.lp.learnandplay.controller;
 
-import com.fasterxml.jackson.databind.JsonSerializer;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import ru.lp.learnandplay.model.test;
+import ru.lp.learnandplay.model.Task;
 
 @RestController
-public class testController {
+public class taskController {
     @GetMapping("/getTask")
-    public test getG() {
-        return new test(1l,1l,"What day is it?", "1",0, null);
+    public Task getG() {
+        return new Task(1l,1l,"What day is it?",null, "1",0, null);
     }
 }
