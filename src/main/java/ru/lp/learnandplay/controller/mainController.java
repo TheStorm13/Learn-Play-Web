54b86932d07e935537e437670a192fdc4fb5c556
ilya_groza <ilya.groza13@gmail.com>
2024-04-11 20:34:24 +0400

    test bd and requests

--- src/main/java/ru/lp/learnandplay/controller/mainController.java
+++ src/main/java/ru/lp/learnandplay/controller/mainController.java
@@ -1,20 +1,28 @@
 package ru.lp.learnandplay.controller;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import ru.lp.learnandplay.model.User;
+import ru.lp.learnandplay.repository.UsersRepository;
 
-@Controller //для работы с RestFul сервисами(http запросы) -> писать необходимые аннотации к методам
+import java.util.List;
+
+
+@RestController
 public class mainController {
-    @GetMapping("/")
-    public String getWelcomePage() {
-        return "home";
-    }
-    //'/education/topic{}tast{}'
-    @GetMapping("/entry")
-    public String getEntryPage() {
-        return "entry";
+    @Autowired
+    private UsersRepository usersRepository;
+
+
+    @PostMapping("/add")
+    public void addUser(@RequestBody User user) {
+        usersRepository.save(user);
     }
 
+    @GetMapping("/getUsers")
+    public List<User> getUser(){
+        return usersRepository.findAll();
+    }
 
 }
