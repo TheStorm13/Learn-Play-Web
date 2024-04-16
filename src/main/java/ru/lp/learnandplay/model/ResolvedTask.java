2042dcf4dc1f9840fb25b77cef239cff43cc44b6
ilya_groza <ilya.groza13@gmail.com>
2024-04-16 22:39:29 +0400

    ResolvedTask to entity

--- src/main/java/ru/lp/learnandplay/model/ResolvedTask.java
+++ src/main/java/ru/lp/learnandplay/model/ResolvedTask.java
@@ -1,30 +1,47 @@
 package ru.lp.learnandplay.model;
 
-public class ResolvedTask {
-    private Long idUser;
-    private Long idTask;
-    private int count=0;
+import jakarta.persistence.*;
 
-    public ResolvedTask(Long idUser, Long idTask, int count) {
-        this.idUser = idUser;
-        this.idTask = idTask;
+@Entity
+@Table(name = "resolved_task")
+public class ResolvedTask {
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "task_id")
+    private Task task;
+
+    @Column(name = "count")
+    private int count;
+
+    public ResolvedTask(User user, Task task, int count) {
+        this.user = user;
+        this.task = task;
         this.count = count;
     }
 
-    public Long getIdUser() {
-        return idUser;
+    public ResolvedTask() {
+
     }
 
-    public void setIdUser(Long idUser) {
-        this.idUser = idUser;
+    public User getUser() {
+        return user;
     }
 
-    public Long getIdTask() {
-        return idTask;
+    public void setUser(User user) {
+        this.user = user;
     }
 
-    public void setIdTask(Long idTask) {
-        this.idTask = idTask;
+    public Task getTask() {
+        return task;
+    }
+
+    public void setTask(Task task) {
+        this.task = task;
     }
 
     public int getCount() {
