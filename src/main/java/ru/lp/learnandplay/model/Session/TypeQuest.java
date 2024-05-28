abe3f6c7948b49dff0dc6dd8054e070fc0cd83c0
ilya_groza <ilya.groza13@gmail.com>
2024-05-28 23:04:54 +0400

    fix session

--- src/main/java/ru/lp/learnandplay/model/Session/TypeQuest.java
+++ src/main/java/ru/lp/learnandplay/model/Session/TypeQuest.java
@@ -1,5 +1,10 @@
 package ru.lp.learnandplay.model.Session;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+@Component
+@SessionScope
 public enum TypeQuest {
     Topic,
     User,
